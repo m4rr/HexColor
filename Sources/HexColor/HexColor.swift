2e8c15ed307d1dd54f80da6c21fae04d1e6f7c35
Marat Say <remarr@gmail.com>
2021-04-18 00:23:43 +0300

    CGFloat-agnostic

--- Sources/HexColor/HexColor.swift
+++ Sources/HexColor/HexColor.swift
@@ -4,21 +4,21 @@ private func has_alpha(_ i: UInt64) -> Bool {
   i & 0xff000000 != 0
 }
 
-private func get_component(order: UInt8 = 0, from hex: UInt64) -> CGFloat {
+private func get_component(order: UInt8 = 0, from hex: UInt64) -> UInt64 {
   let rgb_normalized_to_rgbA = has_alpha(hex) ? hex : hex << 8 // append 00 if no alpha at the end
   let shift = order * 8
   let mask: UInt64 = 0xff << shift
   let hex_component = (rgb_normalized_to_rgbA & mask) >> shift
 
-  return CGFloat(hex_component) / 255
+  return hex_component
 }
 
-private func create_rgba(hex: UInt64) -> (CGFloat, CGFloat, CGFloat, CGFloat) {
+private func create_rgba_t<T: FloatingPoint>(hex: UInt64, of: T.Type) -> (T, T, T, T) {
   (
-      red: get_component(order: 3, from: hex),
-    green: get_component(order: 2, from: hex),
-     blue: get_component(order: 1, from: hex),
-    alpha: (has_alpha(hex) ? get_component(from: hex) : 1)
+    T(get_component(order: 3, from: hex)) / 255,
+    T(get_component(order: 2, from: hex)) / 255,
+    T(get_component(order: 1, from: hex)) / 255,
+    has_alpha(hex) ? T(get_component(from: hex)) / 255 : 1
   )
 }
 
@@ -38,30 +38,9 @@ public extension UInt64 {
 }
 
 #if canImport(AppKit)
-
 import AppKit
 
 extension NSColor {
-  convenience init(hex: uint64) {
-    let rgba = create_rgba(hex: hex)
-
-    self.init(red: rgba.0, green: rgba.1, blue: rgba.2, alpha: rgba.3)
-  }
-
-  convenience init?(hex str: String) {
-    if let hex = parse_hex(str) {
-      self.init(hex: hex)
-    }
-    return nil
-  }
-}
-
-#endif
-
-#if canImport(UIKit)
-import UIKit
-
-public extension UIColor {
   convenience init(hex: UInt64) {
     let rgba = create_rgba(hex: hex)
 
@@ -71,31 +50,55 @@ public extension UIColor {
   convenience init?(hex str: String) {
     if let hex = parse_hex(str) {
       self.init(hex: hex)
+    } else {
+      return nil
     }
-    return nil
   }
 }
+#endif
 
+#if canImport(UIKit)
+import UIKit
+
+public extension UIColor {
+  /// Creates UIColor from 0xabcdef, 0xabcdefaa.
+  /// - Parameter hex: hex number
+  convenience init(hex: UInt64) {
+    let rgba = create_rgba_t(hex: hex, of: CGFloat.self)
+
+    self.init(red: rgba.0, green: rgba.1, blue: rgba.2, alpha: rgba.3)
+  }
+
+  /// Creates UIColor from "0xabcdef", "0xabcdefaa" or "#abcdef".
+  /// - Returns: nil if no number found in the string (parsing failed)
+  /// - Parameter str: hex number as string
+  convenience init?(hex str: String) {
+    if let hex = parse_hex(str) {
+      self.init(hex: hex)
+    } else {
+      return nil
+    }
+  }
+}
 #endif
 
 #if canImport(UIKit) && canImport(SwiftUI)
 import SwiftUI
 
+@available(iOS 13.0, *)
+public extension Color {
+  init(hex: UInt64) {
+    let rgba = create_rgba_t(hex: hex, of: Double.self)
 
-//  public extension Color {
-//    init(hex: UInt64) {
-//      self.init(UIColor(hex: hex))
-//    }
-//
-//    init?(hex: String) {
-//      if let color = UIColor(hex: hex) {
-//        self.init(color)
-//      } else {
-//        return nil
-//      }
-//    }
-//  }
+    self.init(.sRGB, red: rgba.0, green: rgba.1, blue: rgba.2, opacity: rgba.3)
+  }
 
-
+  init?(hex: String) {
+    if let color = UIColor(hex: hex) {
+      self.init(color)
+    } else {
+      return nil
+    }
+  }
+}
 #endif
-
--- Tests/HexColorTests/HexColorTests.swift
+++ Tests/HexColorTests/HexColorTests.swift
@@ -3,33 +3,30 @@ import XCTest
 
 final class HexColorTests: XCTestCase {
 
-  func testExample() {
-
+  @available(iOS 1.0, *)
+  func test_iOS() {
     #if canImport(UIKit)
-
     let appRed = UIColor(red: 0xFF/255, green: 0x3B/255, blue: 0x30/255, alpha: 1)
     let testC2 = UIColor(hex: 0xff3b30)
     let testC3 = UIColor(hex: "#ff3b30")
 
     XCTAssertEqual(testC2, appRed)
-
     XCTAssertNotNil(testC3)
     XCTAssertEqual(testC3, appRed)
+    #endif
+  }
 
-    #elseif canImport(AppKit)
-
+  @available(macOS 1.0, *)
+  func test_macOS() {
+    #if canImport(AppKit)
     let appRed = NSColor(red: 0xFF/255, green: 0x3B/255, blue: 0x30/255, alpha: 1)
     let testC2 = NSColor(hex: 0xff3b30)
     let testC3 = NSColor(hex: "#ff3b30")
 
     XCTAssertEqual(testC2, appRed)
-
     XCTAssertNotNil(testC3)
     XCTAssertEqual(testC3, appRed)
-
     #endif
-
-
   }
 
 }
